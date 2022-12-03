d9caa215f4aba3e3c76f74bb870511a6fec58ec3
maximiliansl <maximilian.slapnik@gmail.com>
2022-12-03 11:40:55 +0100

    add comments for clarity

--- node-backend/app.js
+++ node-backend/app.js
@@ -1,8 +1,8 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 // ##### IMPORTANT
 // ### Your backend project has to switch the MongoDB port like this
@@ -12,10 +12,10 @@ const db = require('monk')(`127.0.0.1:${MONGODB_PORT}/omm-2223`); // connect to 
 console.log(`Connected to MongoDB at port ${MONGODB_PORT}`)
 // ######
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- node-backend/routes/index.js
+++ node-backend/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  // look in the views folder for a file that has the name of index before the extension
   res.render('index', { title: 'Express' });
 });
 
