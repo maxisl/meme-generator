5a5d4a6e1dd82e312bcb06c4cd37318099f25c53
maximiliansl <maximilian.slapnik@gmail.com>
2022-12-17 11:09:43 +0100

    fix index and user routes

--- node-backend/routes/index.js
+++ node-backend/routes/index.js
@@ -4,17 +4,9 @@ var router = express.Router();
 /* TODO define swagger schemas
    https://www.youtube.com/watch?v=S8kmHtQeflo */
 
-/* GET home page. */
-/*router.get('/', function(req, res, next) {
-  res.status(200).json({
-    message: "It works"
-  });
-  // look in the views folder for a file that has the name of index before the extension
-  // res.render('index', { title: 'Express' });
-});*/
-
 // TODO add more routes https://www.youtube.com/watch?v=FV1Ugv1Temg&list=PL55RiY5tL51q4D-B63KBnygU6opNPFk_q&index=3
 
+/* GET home page. */
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Handling GET requests to /index'
--- node-backend/routes/users.js
+++ node-backend/routes/users.js
@@ -2,12 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   const db = req.db;
   const users = db.get('users');
   users.find({username: req.username},{ projection: {basicauthtoken: 0} }) // return all user properties, except the basic auth token
       .then((docs) => res.json(docs))
       .catch((e) => res.status(500).send())
+});*/
+
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Handling GET requests to /users'
+  });
 });
 
 // TODO Implement Basic Routing (see notes)
